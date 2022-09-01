3aad41c9dfbbe78b7fb745cb0d21e07746cdf3a4
Neil Rifarial <111344245+Neillobino18@users.noreply.github.com>
2022-09-02 04:03:27 +0800

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,14 @@
+public class Enemy {
+
+    public static boolean hasMagicImmunity;
+    public static String name;
+    public int damage;
+    public int health;
+    public int armor;
+    public void enemyDialogue() {
+        System.out.println("Fear me player for my name is " + name + " and I deal " + damage + " damage!");
+    
+    
+
+    }
+}
